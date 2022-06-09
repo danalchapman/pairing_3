c6722570d27b15427de6615a3ea3f40e8ff719a2
Dana Chapman <dana.leigh.dlc@gmail.com>
2022-06-09 18:50:15 -0500

    Add Under 60

--- friends.js
+++ friends.js
@@ -3,3 +3,7 @@ var numbers = [22, 36, 12, 97, 62, 45, 78]
 for (var n = 0; n < numbers.length; n++) {
   console.log(numbers[n] > 30);
   }
+
+for (var n = 0; n < numbers.length; n++) {
+  console.log(numbers[n] < 60);
+  }
